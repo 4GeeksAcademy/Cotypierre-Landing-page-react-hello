31b5f483f2f094b1347f7b3ffe41a20247221dbf
CotyPierre <135678561+CotyPierre@users.noreply.github.com>
2023-09-02 03:22:47 +0000

    Landing Page with React

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const footer = () => {
+	return (
+		<div className="text-center">
+			<div class="card">
+                <div class="card-header">
+                    Copy Rith
+                </div>
+                
+            </div>
+		</div>
+	);
+};
+
+export default footer;
